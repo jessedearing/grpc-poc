bbbc376308b0c00d520b4ff4bbbc8c094c05b1e5
Jesse Dearing <jesse.dearing@gmail.com>
2017-01-05 16:06:55 -0800

    Looping and sending fake data

--- node/client.js
+++ node/client.js
@@ -2,21 +2,28 @@
 
 var messages = require('./person_pb.js');
 var service = require('./person_grpc_pb.js');
+var faker = require('faker');
 
 var grpc = require('grpc');
 
-function main() {
-  var client = new service.PersonSenderClient('localhost:3001', grpc.credentials.createInsecure());
-
+function sendRequestLoop(client) {
   var person = new messages.PersonRequest();
-  person.setFirstname('Jesse');
-  person.setLastname('Dearing');
-  person.setEmail('jessedearing@invisionapp.com');
-  person.setIsemployee(true);
+  person.setFirstname(faker.name.firstName());
+  person.setLastname(faker.name.lastName());
+  person.setEmail(faker.internet.email());
+  person.setIsemployee(faker.random.boolean());
 
   client.sendPerson(person, (err, response) => {
     console.log(response.getOk());
   });
+  setTimeout(function() {
+    sendRequestLoop(client);
+  }.bind(this), 1000);
+}
+
+function main() {
+  var client = new service.PersonSenderClient('localhost:3001', grpc.credentials.createInsecure());
+  sendRequestLoop(client);
 }
 
 main();
